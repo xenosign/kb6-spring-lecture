d81179109d7fbebf1f51a48f5d62422735783c79
kdtTetz <xenosign@naver.com>
2025-06-12 05:50:16 +0900

    250612 수업 코드 추가

---
+++ src/main/java/org/example/kb6spring/mapper/MemberMapper.java
@@ -0,0 +1,11 @@
+package org.example.kb6spring.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.example.kb6spring.domain.member.MemberEntity;
+
+import java.util.List;
+
+@Mapper
+public interface MemberMapper {
+    List<MemberEntity> findAll();
+}
