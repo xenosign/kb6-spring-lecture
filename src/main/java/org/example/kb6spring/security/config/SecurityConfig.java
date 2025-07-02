ea9b785c0eef37566b01ce5d96118305337396a5
Tetz <xenosign@naver.com>
2025-07-02 11:34:31 +0900

    Update SecurityConfig.java

--- src/main/java/org/example/kb6spring/security/config/SecurityConfig.java
+++ src/main/java/org/example/kb6spring/security/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.example.kb6spring.security.jwt.JwtTokenProvider;
 import org.example.kb6spring.security.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
