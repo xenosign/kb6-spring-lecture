32663a151229ba1ce0c9a569d7b46a6e97724c13
kdtTetz <xenosign@naver.com>
2025-07-19 03:38:46 +0900

    Create SeatController.java

---
+++ src/main/java/org/example/kb6spring/controller/seat/SeatController.java
@@ -0,0 +1,17 @@
+package org.example.kb6spring.controller.seat;
+
+
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "Todo 컨트롤러")
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/seat")
+public class SeatController {
+}
